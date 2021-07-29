90ab5ba9abf7d8c66ae0e1f9b381cde31e5e468b
anu-aj <anugrahajawahar@gmail.com>
2021-07-29 17:03:45 +0530

    (feat):styling in progress

---
+++ action/action.js
@@ -0,0 +1,21 @@
+ var x, y, s = 0;
+
+ function addition(x, y) {
+     return x + y;
+ }
+
+ function sub(x, y) {
+     return x - y;
+ }
+
+ function mul(x, y) {
+     return x * y;
+ }
+
+ function div(x, y) {
+     return x / y;
+ }
+
+ function calc() {
+     s = s + x;
+ }
